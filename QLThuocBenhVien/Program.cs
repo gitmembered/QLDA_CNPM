137d00a5e51f2f55f37077bd776b310f669f4023
Duyooooo <quocduynew0985@gmail.com>
2026-06-24 02:31:36 +0700

    deploy railway

--- QLThuocBenhVien/Program.cs
+++ QLThuocBenhVien/Program.cs
@@ -1,25 +1,26 @@
 using Microsoft.EntityFrameworkCore;
 using QLThuocBenhVien.Data;
-using System.Text.Json.Serialization; // Điểm thêm 1: Khai báo thư viện xử lý JSON
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// ==========================================
-// 1. ĐĂNG KÝ CÁC DỊCH VỤ (SERVICES)
-// ==========================================
-
-// Cấu hình kết nối CSDL SQL Server qua Entity Framework
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Điểm thêm 2: Thêm dịch vụ hỗ trợ mô hình MVC VÀ chống vòng lặp JSON
 builder.Services.AddControllersWithViews()
     .AddJsonOptions(options =>
     {
         options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
     });
 
-// Điểm thêm 3: Cấu hình CORS (Cho phép mọi thiết bị, tên miền khác truy cập API)
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Account/Login";
+        options.AccessDeniedPath = "/Account/AccessDenied";
+    });
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll",
@@ -33,34 +34,23 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// ==========================================
-// 2. CẤU HÌNH ĐƯỜNG ỐNG XỬ LÝ HTTP (MIDDLEWARE)
-// ==========================================
-
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseRouting();
-
-// Điểm thêm 4: Bật CORS (Lưu ý bắt buộc phải đặt ở giữa UseRouting và UseAuthorization)
 app.UseCors("AllowAll");
-
+app.UseAuthentication();
 app.UseAuthorization();
-
+app.UseStaticFiles();
 app.MapStaticAssets();
 
-// Định tuyến mặc định của MVC
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Account}/{action=Login}/{id?}")
     .WithStaticAssets();
 
-// ==========================================
-// 3. CHẠY ỨNG DỤNG
-// ==========================================
 app.Run();
